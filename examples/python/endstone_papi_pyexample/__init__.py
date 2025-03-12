491194b40a00c63f3695dc6287176f27e17dfbca
Vincent <magicdroidx@gmail.com>
2025-03-12 15:24:46 +0000

    feat: add python example plugin

---
+++ examples/python/endstone_papi_pyexample/__init__.py
@@ -0,0 +1,3 @@
+from .plugin import JoinExample
+
+__all__ = ["JoinExample"]
---
+++ examples/python/endstone_papi_pyexample/plugin.py
@@ -0,0 +1,22 @@
+from endstone_papi import PlaceholderAPI
+from endstone.plugin import Plugin
+from endstone.event import event_handler, EventPriority, PlayerJoinEvent
+
+
+class JoinExample(Plugin):
+    api_version = "0.6"
+    soft_depend = ["papi"]
+
+    def on_enable(self):
+        if self.server.plugin_manager.is_plugin_enabled("papi"):
+            self.register_events(self)
+            self.papi = PlaceholderAPI(self)
+        else:
+            self.logger.warning("Could not find PlaceholderAPI! This plugin is required.")
+            self.server.plugin_manager.disable_plugin(self)
+
+    @event_handler(priority=EventPriority.HIGHEST)
+    def on_player_join(self, event: PlayerJoinEvent):
+        join_text = "{player_name} joined the server! Their game mode is {player_gamemode}"
+        join_text = self.papi.set_placeholder(event.player, join_text)
+        event.join_message = join_text
