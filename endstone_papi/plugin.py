488c9688300fc47890b747711c93d3a70cd3d56f
Vincent <magicdroidx@gmail.com>
2025-03-18 22:04:10 +0000

    fix: correct parameters of `parse` subcommand

--- endstone_papi/plugin.py
+++ endstone_papi/plugin.py
@@ -12,7 +12,7 @@ class PlaceholderAPIPlugin(Plugin):
         "papi": {
             "description": "PlaceholderAPI command",
             "usages": [
-                "/papi parse <text: message> [player: player]",
+                "/papi parse <player: player> <text: message>",
                 "/papi list",
             ],
             "permissions": ["papi.command.papi"],
@@ -43,18 +43,27 @@ class PlaceholderAPIPlugin(Plugin):
     ) -> bool:
         match args[0]:
             case "parse":
-                text: str = args[1]
-                player: Player | None = None
-                if len(args) == 3:
-                    player_name: str = args[2]
-                    player = self.server.get_player(player_name)
-                    if player is None:
-                        sender.send_error_message(
-                            f"Could not find player {player_name}!"
-                        )
-                        return False
+                assert len(args) == 3, f"Invalid number of arguments! Expected 3, got {len(args)}."
+                match args[1]:
+                    case "me":
+                        if not isinstance(sender, Player):
+                            sender.send_error_message("You must be a player to use 'me' as a target!")
+                            return True
 
+                        player = sender
+
+                    case "--null":
+                        player = None
+
+                    case player_name:
+                        player = self.server.get_player(player_name)
+                        if player is None:
+                            sender.send_error_message(f"Could not find player {player_name}!")
+                            return True
+
+                text: str = args[2]
                 sender.send_message(self._api.set_placeholders(player, text))
+
             case "list":
                 sender.send_message("Available placeholders:")
                 for identifier in self._api.registered_identifiers:
